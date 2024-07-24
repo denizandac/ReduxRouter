cda4ac2c79e51c3fe5b7915654fa2c4b8ab764d4
Deniz <denizandac33@gmail.com>
2024-07-24 12:06:05 +0300

    Toggle Counter
    +added toggle for showing the counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,8 +2,8 @@ import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
@@ -12,15 +12,22 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement", amount: 1 });
   };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
-      <div>
-        <button onClick={incrementHandler}>Increment</button>
-        <button onClick={decrementHandler}>Decrement</button>
-      </div>
+      {show && (
+        <div>
+          <h1>Redux Counter</h1>
+          <div className={classes.value}>{counter}</div>
+          <div>
+            <button onClick={incrementHandler}>Increment</button>
+            <button onClick={decrementHandler}>Decrement</button>
+          </div>
+        </div>
+      )}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,32 @@
-const redux = require("redux");
+import { createStore } from "redux";
 
 const initialState = {
   counter: 0,
+  showCounter: true,
 };
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case "increment":
       return {
-        ...state,
         counter: state.counter + action.amount,
+        showCounter: state.showCounter,
       };
     case "decrement":
       return {
-        ...state,
         counter: state.counter - action.amount,
+        showCounter: state.showCounter,
+      };
+    case "toggle":
+      return {
+        counter: state.counter,
+        showCounter: !state.showCounter,
       };
     default:
       return state;
   }
 };
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
